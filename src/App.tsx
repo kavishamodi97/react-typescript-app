27bd62e02962b373e14cdd17a7f5a9f0f27c864f
kavishamodi97 <kavishamodi971@gmail.com>
2021-08-08 11:32:40 +0530

    implement todo form with useref hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
-import './App.css';
+import Todos from './components/Todos';
+import Todo from './models/todo';
 
 function App() {
-  return (
-    <div className="App">
+  const todos = [new Todo('Learn React'), new Todo('Learn TypeScript')];
 
+  return (
+    <div>
+      <Todos items={todos} />
     </div>
   );
 }
