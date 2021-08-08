f528f3f9175813d41e5533dcd630f8ed38504c04
kavishamodi97 <kavishamodi971@gmail.com>
2021-08-08 11:57:40 +0530

    Add And Remove Todo

--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,6 +1,8 @@
 import { useRef } from 'react';
 
-const NewTodo = () => {
+import classes from './NewTodo.module.css';
+
+const NewTodo: React.FC<{ onAddTodo: (text: string) => void }> = (props) => {
     const todoTextInputRef = useRef<HTMLInputElement>(null);
 
     const submitHandler = (event: React.FormEvent) => {
@@ -12,10 +14,12 @@ const NewTodo = () => {
             // throw an error
             return;
         }
+
+        props.onAddTodo(enteredText);
     };
 
     return (
-        <form onSubmit={submitHandler}>
+        <form onSubmit={submitHandler} className={classes.form}>
             <label htmlFor='text'>Todo text</label>
             <input type='text' id='text' ref={todoTextInputRef} />
             <button>Add Todo</button>
